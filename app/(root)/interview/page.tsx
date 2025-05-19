06757c3537ecac91c2ad43b8a9d2a6639a4fc423
YoramMengistu <119293009+YoramMengistu@users.noreply.github.com>
2025-05-19 19:31:01 +0300

    page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 const Page = async () => {
   const user = await getCurrentUser();
-  if (!user || user.name || !user.id) {
-    return <p>Loading or missing user data...</p>;
-  }
+  // if (!user || user.name || !user.id) {
+  //   return <p>Loading or missing user data...</p>;
+  // }
   return (
     <>
       <h3>Interview Generation</h3>
-      <Agent userName={user?.name} userId={user?.id} type="generate" />
+      <Agent userName={user?.name || ""} userId={user?.id} type="generate" />
     </>
   );
 };
